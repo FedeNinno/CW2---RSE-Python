8b9a4c7a35e3450c02d068c3943941b85c2fd47a
FedeNinno <72388112+FedeNinno@users.noreply.github.com>
2021-01-05 22:02:14 +0100

    Second attempt

--- comparesamples.py
+++ comparesamples.py
@@ -3,16 +3,7 @@ from statistics import mean
 #!/usr/bin/env python
 from argparse import ArgumentParser
 
-# read sample files
-
-# Ask the user and store the path in the variables
-#samples1 = 'C:\\Users\\Federica Ninno\\Desktop\\RSECW2\\original_code\\samples1.csv'
-#samples2 = 'C:\\Users\\Federica Ninno\\Desktop\\RSECW2\\original_code\\samples2.csv'
-#weights = 'C:\\Users\\Federica Ninno\\Desktop\\RSECW2\\original_code\\weights.csv'
-
-threshold = 5
-
-def comparesamples(samples1, samples2, weights, summary = 'd', analysis = 'x'):
+def comparesamples(samples1, samples2, weights = 1, summary = 'd', analysis = 'x', threshold = 5):
     
     # Function reading the .csv files (samples and weights data) and prepare data for the analyses
     def data_read (file):
@@ -25,49 +16,41 @@ def comparesamples(samples1, samples2, weights, summary = 'd', analysis = 'x'):
                     row.append(float(string.strip()))
                 data_storage_name.append(row)
         return data_storage_name
-
+    
+    # Recall the function
     data1 = data_read(samples1)
     data2 = data_read(samples2)
-    w = data_read(weights)
-    
+    if weights == 1:
+        w = 1
+    else:
+        w = data_read(weights)
     
     results = []
-# Replace loop with iterator - TODO
     for value in range(len(data1)):
         s = 0
-    
-    # Algorithm X - Sample difference
-    # Replace loop with iterator - TODO
-        if analysis == 'x':
-            for element in range(len(data1)):
-            # Ignore missing values
-                if isnan(data1[value][element]) or isnan(data2[value][element]):
-                    d = 0
-                else:
-                    d = data1[value][element] - data2[value][element]
-                s += w[0][element] * abs(d)
-            results.append(s)
-        
-    # Algorithm Y - Discrepancies
-        if analysis == 'y':
-            for element in range(len(data1)):
-            # Ignore missing values
-                if isnan(data1[value][element]) or isnan(data2[value][element]):
-                    d = 0
-                else:
-                    d = (data1[value][element] - data2[value][element])**2
-                
-                s += w[0][element] * d
-                discr = sqrt(s)
-    
-            results.append(round(discr,2))
-    
-    # Possible adding of other algorithms
-    
+        for element in range(len(data1)):
+            if isnan(data1[value][element]) or isnan(data2[value][element]):
+                data1[value][element] = 0
+                data2[value][element] = 0
 
-    # Criticality - number of elements in the sample difference vector exceeding a threshold value 
-    # Replace loop with iterator
-    # Delete lines in excess
+                if analysis == 'x':
+                    d = data1[value][element] - data2[value][element]
+
+                if analysis == 'y':
+                    d = (data1[value][element] - data2[value][element])**2
+
+                if w == 1:
+                    s += w * abs(d)
+                else:
+                    s += w[0][element] * abs(d)
+
+        if analysis == 'x':          
+            results.append(s)
+        if analysis == 'y':
+            discr = sqrt(s)
+            results.append(round(discr,2))
+                
+# Criticality - number of elements in the sample difference vector exceeding a threshold value 
     critical = 0
     for result in results:
         if result > threshold:
@@ -76,30 +59,23 @@ def comparesamples(samples1, samples2, weights, summary = 'd', analysis = 'x'):
     # d-index - average of sample difference vector across sample pairs
     d_index = mean(results)
     
-    #dsum =  0
-    #for i in range(len(results)):
-       # dsum = dsum + results[i]
-    
-    #index = dsum/len(results)
-
-        #d_index = dsum/len(results)
-        #print("d-index:", dsum/len(results))
-
     if summary == 'criticality':
         return(print("criticality:", critical, "results above", threshold))
     if summary == 'd':
         return(print("d-index:", d_index))
 
+# Command-line interface
 if __name__ == "__main__":
-    parser = ArgumentParser(description="Compute criticality and d-index")
-    parser.add_argument('--summary', '-s')
-    parser.add_argument('--analysis','-a')
-    parser.add_argument('samples1', type=str)
-    parser.add_argument('samples2', type=str)
-    parser.add_argument('weights', type=str)
+    parser = ArgumentParser(description="Compute criticality and d-index of samples of two different locations")
+    parser.add_argument('--summary', '-s', help = 'Desired output: criticality or d-index', default='d')
+    parser.add_argument('--analysis','-a', help = 'Chosen Algorithm (X o Y)',default='x')
+    parser.add_argument('--threshold','-t', help = 'Chosen threshold for criticality calculation', default = 5)
+    parser.add_argument('--weights', '-w', help = 'Weights of the different properties', default = 1)
+    parser.add_argument('samples1', help = 'Data related to the samples of the first location', default = 'samples1.csv')
+    parser.add_argument('samples2', help = 'Data related to the samples of the second location', default = 'samples2.csv')
     arguments= parser.parse_args()
     
-    final = comparesamples(arguments.samples1, arguments.samples2, arguments.weights,
-    arguments.summary, arguments.analysis)
+    final = comparesamples(arguments.samples1, arguments.samples2, 
+      arguments.weights, arguments.summary, arguments.analysis, arguments.threshold)
     
     print(final)
